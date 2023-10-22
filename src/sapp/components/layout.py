9c837cc0ce5eec2368da6371406349ba155dff08
MooSee2 <52461645+MooSee2@users.noreply.github.com>
2023-10-21 21:19:17 -0600

    formatting

--- src/sapp/components/layout.py
+++ src/sapp/components/layout.py
@@ -1,7 +1,30 @@
+from dash import html
+
+
 def make_layout():
-        [
-        html.Div(children="Hello World1", style={"width": "100%", "height": "50px"}, id="box1"),
-        html.Div(children="Hello World2", style={"width": "100%", "height": "50px"}, id="box2"),
-        html.Div(children="Hello World3", style={"width": "100%", "height": "50px"}, id="box3"),
-        html.Div(children="Hello World4", style={"width": "100%", "height": "50px"}, id="box4"),
-    ],
+    return [
+        html.Div(
+            children="Hello World1",
+            style={"width": "100%", "height": "50px"},
+            id="box1",
+            className="box1",
+        ),
+        html.Div(
+            children="Hello World2",
+            style={"width": "100%", "height": "50px"},
+            id="box2",
+            className="box",
+        ),
+        html.Div(
+            children="Hello World3",
+            style={"width": "100%", "height": "50px"},
+            id="box3",
+            className="box",
+        ),
+        html.Div(
+            children="Hello World4",
+            style={"width": "100%", "height": "50px"},
+            id="box4",
+            className="box",
+        ),
+    ]
