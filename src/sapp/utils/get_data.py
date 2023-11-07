5e83cf02d39c31764bc527b2fe345dd07000404c
MooSee2 <52461645+MooSee2@users.noreply.github.com>
2023-11-06 20:54:15 -0700

    good

--- src/sapp/utils/get_data.py
+++ src/sapp/utils/get_data.py
@@ -1,11 +1,134 @@
-import requests
-import multiprocessing
+# from loguru import logger
+# from multiprocessing import Pool
 import pandas as pd
+import requests
 import json
+import re
 
-def get_nwis():
+
+def dump_nwis_to_json(path="test_data.json"):
     url = "https://waterservices.usgs.gov/nwis/dv/?sites=12323233&parameterCd=00060&startDT=2023-10-14&endDT=2023-10-21&format=json"
     response = requests.get(url=url)
     jdata = response.json()
-    with open("test_data.json", mode="w") as f:
+    with open(path, mode="w") as f:
         json.dump(jdata, f)
+
+
+def parse_nwis_json(data) -> pd.DataFrame:
+    """Parse json data from nwis into a pandas DataFrame.
+
+    Parameters
+    ----------
+    data : json
+        json data from nwis.
+
+    Returns
+    -------
+    pd.DataFrame
+        The data
+    """
+    parsed_data = []
+    for series in data["value"]["timeSeries"]:
+        site_data = series["sourceInfo"]
+        site_code = site_data["siteCode"][0]
+        variable = series["variable"]
+        parsed_data.extend(
+            (
+                {
+                    "value": point["value"],
+                    "qualifiers": point["qualifiers"],
+                    "dateTime": point["dateTime"],
+                    "agencyCode": site_code["agencyCode"],
+                    "variableName": variable["variableName"],
+                    "unitCode": variable["unit"]["unitCode"],
+                    "latitude": site_data["geoLocation"]["geogLocation"]["latitude"],
+                    "longitude": site_data["geoLocation"]["geogLocation"]["longitude"],
+                    "siteName": site_data["siteName"],
+                }
+                for point in series["values"][0]["value"]
+            )
+        )
+    return pd.DataFrame(parsed_data)
+
+
+def query_nwis(service: str, **kwargs) -> pd.DataFrame:
+    """Query an nwis service for data.
+    Tested services are: "iv", "dv", and "gwlevels"
+    kwargs are any valid nwis query parameters.
+
+    Parameters
+    ----------
+    service : str
+        "iv" or "dv" or "gwlevels"
+
+    Returns
+    -------
+    pd.DataFrame
+        The data
+    """
+    if "sites" in kwargs and isinstance(kwargs["sites"], list):
+        kwargs["sites"] = ",".join(kwargs["sites"])
+
+    response = requests.get(
+        url=f"https://waterservices.usgs.gov/nwis/{service}",
+        params=kwargs,
+    )
+    return parse_nwis_json(response.json())
+
+
+""""""
+
+
+""" 
+    # parsed_data is the list to return that will go into df = pd.DataFrame(data)
+    parsed_data = []
+    for series in data["value"]["timeSeries"]:
+        # data is a reference to the json data that has been turned into a python dictionary.
+        # We need to go through the list at data["value"]["timeSeries"].
+        # These are the different time-series available from the query.
+        # Each of them is a dictionary.
+        # References to references is faster than making the computer read through each time
+
+        # for point in series["values"][0]["value"]:
+        #     parsed_data.append(
+        #         {
+        #             "value": point["value"],
+        #             "qualifiers": point["qualifiers"],
+        #             "dateTime": point["dateTime"],
+        #             "agencyCode": site_code["agencyCode"],
+        #             "variableName": variable["variableName"],
+        #             "unitCode": variable["unit"]["unitCode"],
+        #             "latitude": site_data["geoLocation"]["geogLocation"]["latitude"],
+        #             "longitude": site_data["geoLocation"]["geogLocation"]["longitude"],
+        #             "siteName": site_data["siteName"],
+        #         }
+        #     )
+Python list extend can accept generator objects as arguments. 
+This means that the list will be extended by the values yielded
+by the generator object, without creating an intermediate list. 
+However, this also means that the generator object will 
+be exhausted after the extend operation, and cannot be reused.
+
+We need a list of dictionaries to pass to pd.DataFrame
+to get the data into a format we can easily manipulate.
+The dictionary keys will be the column names and the values
+will be rows """
+
+
+# def parser_default_none(data: list, result=None):
+#     if result is None:
+#         result = []
+
+#     result.extend(data)
+#     return tuple(result)
+
+
+# def parser_default_list(data: list, result: list = []):
+#     result.extend(data)
+#     return tuple(result)
+
+
+# def parser_no_default(data: list):
+#     result = []
+#     result.extend(data)
+#     return tuple(result)
--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -1,5 +1,197 @@
 from src.sapp.utils import get_data as gd
 
 
-def test_get_nwis():
-    gd.get_nwis()
+def test_parse_nwis_json():
+    data = gd.parse_nwis_json()
+    return
+
+
+def test_query_nwis_gwlevels_1sites():
+    data = gd.query_nwis(
+        "gwlevels",
+        format="json",
+        sites="433613110443501",
+        startDT="2022-01-01",
+        endDT="2023-10-26",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 15
+
+
+def test_query_nwis_gwlevels_2sites():
+    data = gd.query_nwis(
+        "gwlevels",
+        format="json",
+        sites=["433613110443501", "433615110440001"],
+        startDT="2022-01-01",
+        endDT="2023-10-26",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 36
+
+
+def test_query_nwis_dv_1sites_q():
+    data = gd.query_nwis(
+        "dv",
+        format="json",
+        sites="12323233",
+        startDT="2022-01-01",
+        endDT="2022-01-15",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 15
+
+
+def test_query_nwis_dv_2sites_q():
+    data = gd.query_nwis(
+        "dv",
+        format="json",
+        sites=["12323233", "12323242"],
+        startDT="2022-01-01",
+        endDT="2022-01-15",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 30
+
+
+def test_query_nwis_dv_1sites_q_turb_temp():
+    data = gd.query_nwis(
+        "dv",
+        format="json",
+        sites="12323233",
+        startDT="2022-7-01",
+        endDT="2022-07-15",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 93
+
+
+def test_query_nwis_dv_2sites_q_turb_temp():
+    data = gd.query_nwis(
+        "dv",
+        format="json",
+        sites=["12323233", "12323242"],
+        startDT="2022-7-01",
+        endDT="2022-07-15",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 180
+
+
+def test_query_nwis_iv_1sites_q():
+    data = gd.query_nwis(
+        "iv",
+        format="json",
+        sites="12323233",
+        startDT="2022-1-01",
+        endDT="2022-01-02",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 384
+
+
+def test_query_nwis_iv_2sites_q():
+    data = gd.query_nwis(
+        "iv",
+        format="json",
+        sites=["12323233", "12323242"],
+        startDT="2022-1-01",
+        endDT="2022-01-02",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 768
+
+
+def test_query_nwis_iv_1sites_q_turb_temp():
+    data = gd.query_nwis(
+        "iv",
+        format="json",
+        sites="12323233",
+        startDT="2022-07-01",
+        endDT="2022-07-02",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 768
+
+
+def test_query_nwis_iv_2sites_q_turb_temp():
+    data = gd.query_nwis(
+        "iv",
+        format="json",
+        sites=["12323233", "12323242"],
+        startDT="2022-07-01",
+        endDT="2022-07-02",
+        siteStatus="all",
+        agencyCd="USGS",
+    )
+    assert data.empty is False
+    assert len(data) == 1422
+
+
+# url = "https://waterservices.usgs.gov/nwis/gwlevels/?format=json&sites=433613110443501,433615110440001&startDT=2022-01-01&endDT=2023-10-26&siteStatus=all&agencyCd=USGS"
+# response = requests.get(url)
+
+# if response.status_code == 200:
+#     data = response.json()
+# def test_parser_default_none():
+#     expected = (1, 2, 3, 4)
+#     input_data = [1, 2, 3, 4]
+#     output1 = gd.parser_default_none(input_data)
+#     output2 = gd.parser_default_none(input_data)
+#     output3 = gd.parser_default_none(input_data)
+#     output4 = gd.parser_default_none(input_data)
+
+#     assert output1 == expected
+#     assert output2 == expected
+#     assert output3 == expected
+#     assert output4 == expected
+
+
+# def test_parser_default_list():
+#     expected1 = (1, 2, 3, 4)
+#     expected2 = (1, 2, 3, 4, 1, 2, 3, 4)
+#     expected3 = (1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
+#     expected4 = (1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
+
+#     input_data = [1, 2, 3, 4]
+#     output1 = gd.parser_default_list(input_data)
+#     output2 = gd.parser_default_list(input_data)
+#     output3 = gd.parser_default_list(input_data)
+#     output4 = gd.parser_default_list(input_data)
+
+#     assert output1 == expected1
+#     assert output2 == expected2
+#     assert output3 == expected3
+#     assert output4 == expected4
+
+
+# def test_parser_no_default():
+#     expected = (1, 2, 3, 4)
+#     input_data = [1, 2, 3, 4]
+#     output1 = gd.parser_no_default(input_data)
+#     output2 = gd.parser_no_default(input_data)
+#     output3 = gd.parser_no_default(input_data)
+#     output4 = gd.parser_no_default(input_data)
+
+#     assert output1 == expected
+#     assert output2 == expected
+#     assert output3 == expected
+#     assert output4 == expected
