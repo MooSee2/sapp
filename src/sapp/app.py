66ff352130060559aaac3d9953420a5f0692ce43
MooSee2 <52461645+MooSee2@users.noreply.github.com>
2023-11-06 20:52:41 -0700

    add todo

--- src/sapp/app.py
+++ src/sapp/app.py
@@ -3,6 +3,9 @@ from loguru import logger
 from dash import Dash, html
 from components.layout import make_layout
 from dotenv import load_dotenv
+# from flask_cache import Cache
+
+# TODO caching and performance: https://dash.plotly.com/performance
 
 load_dotenv()
 logger.debug(f"ENV DATA_SOURCE value:  {os.getenv('DATA_SOURCE')}")
